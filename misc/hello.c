266f1970b8fa823566729fe59d447be6bcdffd92
Chase Mack <cmack8@huskers.unl.edu>
2022-08-25 20:21:59 +0000

    initial commit

---
+++ misc/hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Chase Mack
+ * 2022/8/23
+ *
+ * A simple hello world program in c
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("hello world!\n");
+
+    return 0;
+ }
